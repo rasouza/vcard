a1436173e760bd1c2f95733210e56b0e26b4e5a5
R. A. Souza <rasouzas@linux.ime.usp.br>
2017-09-27 19:24:46 -0300

    encodes portfolio object in json

--- php/portfolio.php
+++ php/portfolio.php
@@ -3,9 +3,44 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Goutte\Client;
 
+class Portfolio {
+	public $title;
+	public $link;
+	public $image;
+
+}
+
+
 $client = new Client();
 $crawler = $client->request('GET', 'https://rasouza.com.br/portfolio');
 
-$crawler->filter('div.portfolio-item')->each(function ($item) {
-    echo $item->text() . "\n\n";
+$port = [];
+$i = 0;
+$j = 0;
+$k = 0;
+$crawler->filter('div.portfolio-item')->each(function ($item) use (&$port, &$i, &$j, &$k) {
+
+	
+	$item->filter('a')->each(function ($link) use (&$port, &$i) {
+		$obj = new Portfolio();
+		$obj->link = $link->attr('href');
+		$port[$i] = $obj;
+		$i++;
+	});
+
+
+	$item->filter('div.portfolio-item-top img')->each(function ($img) use (&$port, &$j) {
+		$port[$j]->image = 'https:' . $img->attr('data-src');
+		$j++;
+	});
+
+
+
+	$item->filter('.portfolio-item-info h5')->each(function ($title) use (&$port, &$k) {
+		$port[$k]->title = $title->text();
+		$k++;
+	});
+    
 });
+
+echo json_encode($port);
